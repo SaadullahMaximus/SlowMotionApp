c4a489bab8e56673f82cbc984372461731241891
SaadUllah <saadullahmaximustech@gmail.com>
2023-06-02 16:30:28 +0500

    Layout ready to go :)

--- app/src/main/java/com/example/slowmotionapp/ui/fragments/CropSpeedFragment.kt
+++ app/src/main/java/com/example/slowmotionapp/ui/fragments/CropSpeedFragment.kt
@@ -19,6 +19,7 @@ class CropSpeedFragment : Fragment() {
 
     private var childFragmentManager: FragmentManager? = null
     private var currentChildFragment: Fragment? = null
+    private var effectMusicFragment: Fragment? = null
 
 
     override fun onCreateView(
@@ -30,40 +31,37 @@ class CropSpeedFragment : Fragment() {
         childFragmentManager = getChildFragmentManager()
 
         currentChildFragment = MainFragment()
+        effectMusicFragment = EffectMusicFragment()
+
+        binding.backBtn.setOnClickListener {
+            requireActivity().finish()
+        }
+
         childFragmentManager!!.beginTransaction()
             .add(R.id.fragment_container_main, currentChildFragment as MainFragment)
             .commit()
+
+        binding.enhanceBtn.setOnClickListener {
+            binding.enhanceBtn.visibility = View.GONE
+            binding.backTextBtn.visibility = View.VISIBLE
+            childFragmentManager!!.beginTransaction()
+                .replace(R.id.fragment_container_main, effectMusicFragment as EffectMusicFragment)
+                .commit()
+        }
+
+        binding.backTextBtn.setOnClickListener {
+            binding.enhanceBtn.visibility = View.VISIBLE
+            binding.backTextBtn.visibility = View.GONE
+            childFragmentManager!!.beginTransaction()
+                .replace(R.id.fragment_container_main, currentChildFragment as MainFragment)
+                .commit()
+        }
 
 
         val videoUri: String = (activity as EditorActivity?)!!.getTrimmedPath()
         Log.d("Hello", "onCreateView: $videoUri")
 //        binding.trimVideoView.setVideoURI(Uri.parse(videoUri))
 //        binding.trimVideoView.start()
-
-//        binding.knobView.setInitialKnobValue(6)
-
-//        val knobAnimator = ValueAnimator.ofFloat(binding.knobView.knobPositionX, 700F)
-//        knobAnimator.addUpdateListener { animator ->
-//            val animatedValue = animator.animatedValue as Float
-//            binding.knobView.knobPositionX = animatedValue
-//        }
-//        // Set the interpolator for the bounce effect
-//        knobAnimator.interpolator = BounceInterpolator()
-//
-//        // Set the duration of the animation in milliseconds
-//        knobAnimator.duration = 1000
-//
-//        // Start the animation
-//        knobAnimator.start()
-
-
-//        binding.knobView.setOnKnobPositionChangeListener(object : KnobView.OnKnobPositionChangeListener {
-//            override fun onKnobPositionChanged(knobValue: Int) {
-//                // Handle the knob value change here
-//                // You can display it in a TextView or perform any other actions
-//                Log.d("Knob", "onKnobPositionChanged: $knobValue")
-//            }
-//        })
 
 
         return binding.root
@@ -73,35 +71,5 @@ class CropSpeedFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
-//    // Create the bounce animation
-//    fun createBounceAnimation(view: View) {
-//        val animatorSet = AnimatorSet()
-//
-//        // Translation animation
-//        val translationX = ObjectAnimator.ofFloat(view, "translationX", -200f, 0f)
-//        translationX.repeatCount = 2
-//        translationX.repeatMode = ObjectAnimator.REVERSE
-//
-//        // Scale animation
-//        val scaleX = ObjectAnimator.ofFloat(view, "scaleX", 1f, 1.2f, 0.9f, 1.1f, 1f)
-//        val scaleY = ObjectAnimator.ofFloat(view, "scaleY", 1f, 0.8f, 1.1f, 0.9f, 1f)
-//
-//        // Set interpolator for the animations
-//        translationX.interpolator = BounceInterpolator()
-//        scaleX.interpolator = BounceInterpolator()
-//        scaleY.interpolator = BounceInterpolator()
-//
-//        // Set the duration for the animations
-//        translationX.duration = 500
-//        scaleX.duration = 500
-//        scaleY.duration = 500
-//
-//        // Add the animations to the animator set
-//        animatorSet.playTogether(translationX, scaleX, scaleY)
-//
-//        // Start the animation
-//        animatorSet.start()
-//    }
 
 }
